7cdbe7cd855d97adb08be5243ddab527b7f8b938
Priscila Padilla Latance <priscila.latance@gmail.com>
2023-01-01 20:22:33 -0300

    Backend project created

---
+++ backend/src/test/java/com/devsuperior/clientcatalog/ClientcatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.clientcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientcatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
